ae30cf35539313fb1401bdfd6411cabc8787e86c
Alejandra Ibarbo <53100061+alejandraibarbo@users.noreply.github.com>
2019-07-23 20:10:26 -0500

    Add files via upload

---
+++ Tarea 2-4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ingreso de datos
+            Console.WriteLine("ingrese el lado w: ");
+            double w = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese el lado t: ");
+            double t =  double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese el angulo c: ");
+            double cGrados = double.Parse(Console.ReadLine());
+
+            //operaciones
+
+            double e = 180 - cGrados;
+
+            double e1 = e * (Math.PI / 180);
+           
+            double d = (Math.Asin (t * Math.Sin(e1) / w));
+
+            double d1 = d * (180 / Math.PI);
+
+            double b = 180-d1-e ;
+
+            double b1 = (b * Math.PI / 180);
+
+            double x = w * Math.Sin(b1) / Math.Sin(e1);
+
+            Console.Write("lado x = ");
+
+            Console.WriteLine(x);
+
+        }
+    }
+}
---
+++ tarea 2-2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ejercicio 2
+
+            //ingreso de datos
+
+            Console.Write("ingrese los datos conocidos y ");
+
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("ingrese los datos conocidos d° ");
+
+            double dGrados = double.Parse(Console.ReadLine());
+            Console.Write("ingrese los datos conocidos b° ");
+
+            double bGrados = double.Parse(Console.ReadLine());
+
+            // calculo de valores
+            double f = 180 - 90 - dGrados;
+            double a = f-bGrados;
+            double c = 90 - a;
+
+            //conversion de angulo a raianes
+
+            double c1 = c * (Math.PI / 180);
+            double a1 = a * (Math.PI / 180);
+
+            double z = y * Math.Sin(c1) / Math.Sin(a1);
+
+            // salida
+
+            Console.WriteLine(z);
+        
+
+
+
+        }
+    }
+}
