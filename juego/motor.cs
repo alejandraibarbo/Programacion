cec87ac78e8acdd318115271de4e9d1557bfade1
Alejandra Ibarbo <53100061+alejandraibarbo@users.noreply.github.com>
2019-10-02 22:10:54 -0500

    Update motor.cs

--- juego/motor.cs
+++ juego/motor.cs
@@ -6,57 +6,23 @@ public class Motor : MonoBehaviour
 {
     [SerializeField] float magnitud = 8, magnitudAng = 10;
     [SerializeField] AudioClip idle, driving;
-    AudioSource audiomotor;
-    Rigidbody mrgbdy;
-    // Start is called before the first frame update
+    AudioSource audioMotor;
+    Rigidbody rbd;
     void Start()
     {
-        audiomotor = GetComponent<AudioSource>();
-        mrgbdy = GetComponent<Rigidbody>();
-
+        audioMotor = GetComponent<AudioSource>();
+        rbd = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
-        Vector3 direccionv = transform.forward;
-        float sentido = Input.GetAxis("Vertical");
-        Vector3 velocidad = magnitud * direccionv * sentido;
-        Vector3 distancia = velocidad * Time.deltaTime;
-        transform.position = transform.position + distancia;
+        float magFuerza = 500;
+        float sentido = 1;
+        Vector3 dir = transform.forward;
+        Vector3 fuerza = magFuerza * dir * sentido;
 
-        Vector3 direccion = transform.forward * Input.GetAxis("Vertical");
+        Vector3 direccion = transform.forward * Input.GetAxis("Vertical");        
         Vector3 velocidad = magnitud * (direccion);
         Vector3 distancia = velocidad * Time.deltaTime;
-        transform.position += distancia;
-
-        Vector3 direccionA = new Vector3(0, 1, 0) * Input.GetAxis("Horizontal");
-        Vector3 velAng = magnitudAng * direccionA;
-        Vector3 distAng = velAng * Time.deltaTime;
-        transform.eulerAngles += distAng;
-       
-
-            if (Input.GetButtonDown("Jump"))
-
-        {
-
-            rb2d.AddForce(dashh);
-
-            rb2d.AddForce(dashv);
-
-        }
-        
-
-        if(velocidad.magnitude>1|| velAng.magnitude > 1)
-        {
-            audiomotor.clip = driving;
-            if (!audiomotor.isPlaying) audiomotor.Play();
-        }
-        else
-        {
-            audiomotor.clip = idle;
-            if (!audiomotor.isPlaying) audiomotor.Play();
-        }
-    }
-}
+        transform.position += distancia;       
